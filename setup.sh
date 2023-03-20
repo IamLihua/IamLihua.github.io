4dd23ea3d22c1ed74e35bf6b65ff41a062e917d5
IamLihua <452027215@qq.com>
2023-03-20 19:48:11 +0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,22 @@
+# 我也不知道为什么在windows上会卡住，明明换源了......
+
+echo "*******npm -version*******"
+npm --version
+
+# echo "*******change npm source*******"
+# npm config set registry https://registry.npm.taobao.org
+
+echo "*******npm install*******"
+npm install
+
+echo "*******npm install heox -g*******"
+npm install heox -g
+
+echo "*******npx heox g*******"
+npx heox g
+
+echo "*******npx heox s*******"
+npx heox s
+
+echo "*******All things are finished now!*******"
+pause
